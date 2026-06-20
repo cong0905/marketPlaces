5ff81cbbe077c9932a09e2ad42c0de1ca2bb72aa
cong0905 <nguyenduccong09052005@gmail.com>
2026-06-20 19:03:52 +0700

    Add storage:link to startup script to fix broken images in Docker deployment

--- deploy/start.sh
+++ deploy/start.sh
@@ -7,6 +7,9 @@ sed -i "s/listen \[::\]:80;/listen [::]:${PORT:-80};/g" /etc/nginx/sites-enabled
 # Xóa cache Laravel
 php artisan optimize:clear
 
+# Tạo symlink cho thư mục chứa ảnh (Sửa lỗi ảnh 404)
+php artisan storage:link
+
 # Chạy migrations (Bắt buộc phải có cờ --force trong môi trường production)
 php artisan migrate --force
 
